defc3da484b222f3c84f02f6a5c2a1026f8754bc
Bjørn Lindholm <bjornlindholmhansen@gmail.com>
2023-06-21 00:17:00 +0200

    Compile each command

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,9 +2,14 @@ import { build } from "esbuild";
 // import alias from "esbuild-plugin-path-alias";
 // import { copy } from "esbuild-plugin-copy";
 import path from "path";
+import fs from "fs";
+
+const commandPaths = fs
+    .readdirSync(path.resolve(__dirname, "../src/Commands"))
+    .map(fileName => path.resolve(__dirname, "../src/Commands", fileName));
 
 build({
-    entryPoints: ["src/index.ts"],
+    entryPoints: [...commandPaths],
     bundle: true,
     platform: "node",
     target: ["node10.4"],
