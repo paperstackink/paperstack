f7a3012fe2f2319e9f6c5e8fce35f6251e490ce0
Bjørn Lindholm <bjornlindholmhansen@gmail.com>
2023-07-03 22:56:53 +0200

    Add 'make:page' command

--- src/Utilities/Strings.ts
+++ src/Utilities/Strings.ts
@@ -1,4 +1,4 @@
-import { kebabCase } from "lodash";
+import { kebabCase, camelCase, startCase } from "lodash";
 
 export function split(input: string, delimiter: string): string[] {
     // Filter out empty part of delimiter
@@ -7,6 +7,10 @@ export function split(input: string, delimiter: string): string[] {
 
 export function kebab(input: string): string {
     return kebabCase(input);
+}
+
+export function pascal(input: string): string {
+    return startCase(camelCase(input)).replace(/ /g, "");
 }
 
 export function prepend(input: string, prepend: string): string {
